868b56d656c71a653eb0403bcb0d63cd871e9e1f
trishanghosh-PC <ghoshtrishan73@gmail.com>
2018-04-27 21:35:33 +0530

    added gitignore

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -2,26 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = await  _context.Values.ToListAsync();
             // throw new Exception("Test Connection");
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value =await _context.Values.FirstOrDefaultAsync(vl => vl.Id == id);
+            return Ok(value);
         }
 
         // POST api/values
