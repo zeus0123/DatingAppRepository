5ad38b6398a31b58f5a9dc1f9485846c6aaf370a
trishanghosh-PC <ghoshtrishan73@gmail.com>
2018-04-30 12:52:21 +0530

    created the frontend added bootstrap and font awesome and cors

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -27,6 +27,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +38,9 @@ namespace DatingApp.API
                 app.UseDeveloperExceptionPage();
             }
 
+             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
+           
         }
     }
 }
