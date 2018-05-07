6cda916a823767547ff5179c57db8c4049b51624
trishanghosh-PC <ghoshtrishan73@gmail.com>
2018-05-07 23:32:42 +0530

    Added authentication and authorization

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(8 ,MinimumLength = 4,ErrorMessage = "You must specify a password between 4 and 8 characters")]
+        public string Password { get; set; }
+    }
+}
